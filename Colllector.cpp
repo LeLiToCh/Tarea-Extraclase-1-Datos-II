e2ea65f8b51d9264754637768d9a51b0f8a111c8
EmilioTec10 <110855627+EmilioTec10@users.noreply.github.com>
2023-03-04 15:53:54 -0600

    Prueba de la lista enlazada

---
+++ Colllector.cpp
@@ -0,0 +1,16 @@
+class Colllector
+{
+private:
+    /* data */
+public:
+    Colllector(/* args */);
+    ~Colllector();
+};
+
+Colllector::Colllector(/* args */)
+{
+}
+
+Colllector::~Colllector()
+{
+}
